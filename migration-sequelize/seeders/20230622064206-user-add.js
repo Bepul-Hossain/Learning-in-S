6a66c916e0d471330eafb153a6642feeaefc202a
Bepul-Hossain <bepul.cse.ru@gmail.com>
2023-06-22 14:55:07 +0600

    sequelize tutorial done

---
+++ migration-sequelize/seeders/20230622064206-user-add.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    return await queryInterface.bulkInsert('Users', [{
+      name: 'John Doe',
+      createdAt: Sequelize.fn("Now"),
+      updatedAt: Sequelize.fn("NOW")
+     }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
