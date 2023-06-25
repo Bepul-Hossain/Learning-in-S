7ade28538721a0d197fe0550a3feb9dfd73ceaac
Bepul-Hossain <bepul.cse.ru@gmail.com>
2023-06-25 19:46:33 +0600

    create multi table

--- auth-crud-assign/server.js
+++ auth-crud-assign/server.js
@@ -21,52 +21,36 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'Bepul@3964',
-  database: 'auth-crud'
+  database: 'auth-crud',
+  multipleStatements: true
 })
-
-
-
-
-let queryPromise = (params)=>{
-  return new Promise((resolve, reject)=>{
-    connection.query(params, (err, result)=>{
-      if(err){
-         reject(err);
-      }
-      resolve(result);
-    });
-  });
-}
-
 
 
 app.post('/createAuthProfile', (req, res)=>{
 
-    const createAuthQuery = `CREATE TABLE Auth (
+     const createAuthAndProfileQuery = `
+      DROP TABLE IF EXISTS Auth;
+      CREATE TABLE Auth (
         email varchar(255),
         pass varchar(255),
         PRIMARY KEY (email)
+      );
+
+      
+      DROP TABLE IF EXISTS Profiles;
+      CREATE TABLE Profiles (
+        email varchar(255),
+        first_name varchar(255),
+        last_name varchar(255),
+        Age int,PRIMARY KEY (email)
       );`
 
-    const createProfileQuery = `CREATE TABLE Profiles (
-      email varchar(255),
-      first_name varchar(255),
-      last_name varchar(255),
-      Age int,PRIMARY KEY (email)
-    );`
-
-    queryPromise(createAuthQuery)
-    .then((result)=>{
-      console.log("result: ", result);
-      return queryPromise(createProfileQuery);
-    })
-    .then((result)=>{
-      console.log("result 2: "+result);
-      res.send({"result ": result})
-    })
-    .catch((err)=>{
-      res.send(err);
-    })  
+      connection.query(createAuthAndProfileQuery, (err, result)=>{
+        if(err) {
+          res.send(err)
+        };
+        res.send({result});
+      });
 })
 
 
