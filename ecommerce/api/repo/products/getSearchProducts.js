633bb4093608b5e434dad31d4a99d247942e80c4
Bepul-Hossain <bepul.cse.ru@gmail.com>
2023-07-17 18:18:06 +0600

    a

--- ecommerce/api/repo/products/getSearchProducts.js
+++ ecommerce/api/repo/products/getSearchProducts.js
@@ -5,7 +5,17 @@ const getSearchProducts =  async (keyword)=>{
  
     const products = await db.products;
     
-    const result =  await products.findAll({where:{name:{[Op.substring]: keyword}}});
+    const result =  await products.findAll(
+        {
+            where:
+            {
+                name:
+                {
+                    [Op.substring]: keyword
+                }
+            }
+        }
+    );
     
     return result;
 
