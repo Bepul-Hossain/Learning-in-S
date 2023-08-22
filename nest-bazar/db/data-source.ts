4251f938e25905a3362f55ff9c17a9836f63994a
bepul-sharetrip <bepul@sharetrip.net>
2023-08-22 16:00:15 +0600

    a

---
+++ nest-bazar/db/data-source.ts
@@ -0,0 +1,20 @@
+import {DataSource, DataSourceOptions} from 'typeorm'
+import {config} from 'dotenv'
+// import { UserEntity } from 'src/users/entities/user.entity';
+config()
+export const dataSourceOptions: DataSourceOptions = {
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [`${__dirname}/dist/**/*.entity.{ts,js}`],
+    migrations:[`${__dirname}/db/migrations/*.ts`],
+    logging: false,
+    synchronize: false,
+
+}
+
+const dataSource = new DataSource(dataSourceOptions);
+export default dataSource;
