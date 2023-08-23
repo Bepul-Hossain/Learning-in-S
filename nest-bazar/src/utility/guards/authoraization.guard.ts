a7773fd742e2ce9f223063f552bd02272c0f247a
bepul-sharetrip <bepul@sharetrip.net>
2023-08-23 12:34:22 +0600

    improve authorization

--- nest-bazar/src/utility/guards/authoraization.guard.ts
+++ nest-bazar/src/utility/guards/authoraization.guard.ts
@@ -1,21 +1,37 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException, mixin } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 
-@Injectable()
-export class AuthorizeGuard implements CanActivate{
+// @Injectable()
+// export class AuthorizeGuard implements CanActivate{
 
-    constructor(private reflector: Reflector){}
+//     constructor(private reflector: Reflector){}
 
-    canActivate(context: ExecutionContext): boolean {
-        const allowedRoles = this.reflector.get<string[]>('allowedRoles', context.getHandler());
+//     canActivate(context: ExecutionContext): boolean {
+//         const allowedRoles = this.reflector.get<string[]>('allowedRoles', context.getHandler());
 
-        const request = context.switchToHttp().getRequest();
-        const role = request?.currentUser?.roles;
+//         const request = context.switchToHttp().getRequest();
+//         const role = request?.currentUser?.roles;
 
-        const result = allowedRoles.includes(role)
+//         const result = allowedRoles.includes(role)
 
-        if(result) return true;
-        throw new UnauthorizedException('Sorry, you are not authorized admin')
+//         if(result) return true;
+//         throw new UnauthorizedException('Sorry, you are not authorized admin')
     
+//     }
+// }
+
+export const AuthorizeGuard = (allowedRoles:string[])=>{
+    class RolesGuardMixin implements CanActivate{
+        canActivate(context: ExecutionContext): boolean {
+            const request = context.switchToHttp().getRequest();
+            const role = request?.currentUser?.roles;
+
+            const result = allowedRoles.includes(role)
+
+            if(result) return true;
+            throw new UnauthorizedException('Sorry, you are not authorized admin')
+        }
     }
+    const guard = mixin(RolesGuardMixin)
+    return guard;
 }
