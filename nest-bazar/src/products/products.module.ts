ea220477cca7fe601fedf27e45521e26747ae79e
bepul-sharetrip <bepul@sharetrip.net>
2023-08-24 15:01:19 +0600

    views

--- nest-bazar/src/products/products.module.ts
+++ nest-bazar/src/products/products.module.ts
@@ -10,6 +10,7 @@ import { CategoriesModule } from 'src/categories/categories.module';
 
   controllers: [ProductsController],
   providers: [ProductsService],
+  exports: [ProductsService]
 
 })
 export class ProductsModule {}
