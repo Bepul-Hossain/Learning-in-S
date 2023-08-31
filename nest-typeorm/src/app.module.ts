0ca07c3cc40f1169e0d1e8e21eb675e1b294bc74
bepul-sharetrip <bepul@sharetrip.net>
2023-08-31 15:10:25 +0600

    a

--- nest-typeorm/src/app.module.ts
+++ nest-typeorm/src/app.module.ts
@@ -2,18 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/User';
 import { UsersModule } from './users/users.module';
-import { Profile } from './entities/Profile';
 import { ProductsModule } from './products/products.module';
-import { Product } from './entities/Product';
-import { Register } from './entities/Register';
 import { RegisterModule } from './registers/registers.module';
 import { UserRelationsModule } from './user-relations/user-relations.module';
 import { ProfileRelationsModule } from './profile-relations/profile-relations.module';
-import { ProfEntity } from './profile-relations/entities/profile-relation.entity';
-import { UserRelationEntity } from './user-relations/entities/user-relation.entity';
-
+import * as path from 'path';
 @Module({
   imports: [TypeOrmModule.forRoot({
     type:'mysql',
@@ -22,7 +16,7 @@ import { UserRelationEntity } from './user-relations/entities/user-relation.enti
     username: 'root',
     password: 'Bepul@3964',
     database:'nesttypeorm',
-    entities:[User, Profile, Product, Register, ProfEntity, UserRelationEntity],
+    entities:[path.resolve(__dirname, '**/*.entity{.ts,.js}'),],
     synchronize: true
   }), UsersModule, ProductsModule, RegisterModule, UserRelationsModule, ProfileRelationsModule],
   controllers: [AppController],
