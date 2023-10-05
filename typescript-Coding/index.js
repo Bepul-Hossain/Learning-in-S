3727e9a08c71d5ab6cde8828320cdeb7fde32a2e
bepul-sharetrip <bepul@sharetrip.net>
2023-10-05 18:21:35 +0600

    a

--- typescript-Coding/index.js
+++ typescript-Coding/index.js
@@ -1,35 +1,15 @@
-function canCompleteCircuit(A, B) {
-    var n = A.length;
-    for (var i = 0; i < n; i++) {
-        var amountOfGas = A[i];
-        var neededAmountGas = B[i];
-        var currentAmountGas = amountOfGas - neededAmountGas;
-        if (currentAmountGas >= 0) {
-            var count = 0;
-            for (var j = i; j < n; j++) {
-                currentAmountGas = A[j] + currentAmountGas - B[j];
-                if (currentAmountGas < 0) {
-                    break;
-                }
-                else {
-                    count++;
-                }
-            }
-            for (var k = 0; k < i; k++) {
-                currentAmountGas = A[k] + currentAmountGas - B[k];
-                if (currentAmountGas < 0) {
-                    break;
-                }
-                else {
-                    count++;
-                }
-            }
-            if (count >= n) {
-                console.log(n);
-                return i;
-            }
+function removeKdigits(num, k) {
+    var stack = [];
+    var n = num.length;
+    var current = num[0];
+    for (var i = 1; i < n; i++) {
+        if (num[i] >= current) {
+            stack.push(current);
+            current = num[i];
+        }
+        else {
         }
     }
-    return -1;
 }
-console.log(canCompleteCircuit([959, 329, 987, 951, 942, 410, 282, 376, 581, 507, 546, 299, 564, 114, 474, 163, 953, 481, 337, 395, 679, 21, 335, 846, 878, 961, 663, 413, 610, 937, 32, 831, 239, 899, 659, 718, 738, 7, 209], [862, 783, 134, 441, 177, 416, 329, 43, 997, 920, 289, 117, 573, 672, 574, 797, 512, 887, 571, 657, 420, 686, 411, 817, 185, 326, 891, 122, 496, 905, 910, 810, 226, 462, 759, 637, 517, 237, 884]));
+;
+console.log(removeKdigits("14301620", 4));
--- typescript-Coding/index.ts
+++ typescript-Coding/index.ts
@@ -1,38 +1,15 @@
-function canCompleteCircuit(A: number[], B: number[]): number {
-    let n = A.length;
-    for (let i = 0; i < n; i++) {
-        let amountOfGas = A[i];
-        let neededAmountGas = B[i];
-        let currentAmountGas = amountOfGas - neededAmountGas;
-        if (currentAmountGas >= 0) {
-            let count = 0;
-            for (let j = i; j < n; j++) {
-                currentAmountGas = A[j] + currentAmountGas - B[j];
-                if (currentAmountGas < 0) {
-                    break;
-                } else {
-                    count++;
-                }
-            }
-            for (let k = 0; k < i; k++) {
-                currentAmountGas = A[k] + currentAmountGas - B[k];
-                if (currentAmountGas < 0) {
-                    break;
-                } else {
-                    count++;
-                }
-            }
-            if (count >= n) {
-                console.log(n);
-                
-                return i;
-            }
+function removeKdigits(num: string, k: number): string {
+    let stack = [];
+    let n = num.length;
+    let current = num[0];
+    
+    for (let i = 1; i < n; i++) {
+        if(num[i]>=current){
+            stack.push(current);
+            current = num[i];
+        }else{
+
         }
     }
-    return -1;
-}
-
-console.log(canCompleteCircuit(
-    [959, 329, 987, 951, 942, 410, 282, 376, 581, 507, 546, 299, 564, 114, 474, 163, 953, 481, 337, 395, 679, 21, 335, 846, 878, 961, 663, 413, 610, 937, 32, 831, 239, 899, 659, 718, 738, 7, 209],
-    [862, 783, 134, 441, 177, 416, 329, 43, 997, 920, 289, 117, 573, 672, 574, 797, 512, 887, 571, 657, 420, 686, 411, 817, 185, 326, 891, 122, 496, 905, 910, 810, 226, 462, 759, 637, 517, 237, 884]
-));
+};
+console.log(removeKdigits("14301620", 4));
