d30723eedabb5dccc4430a16e750dd2545c52de1
Bepul-Hossain <bepul.cse.ru@gmail.com>
2023-12-07 18:05:38 +0600

    starting graphQL

---
+++ graphQL/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { schema } = require('./graphql/schema');
+const app = express();
+
+app.use("/graphql", graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}))
+
+app.listen(3000, console.log("running on port 3000"));
