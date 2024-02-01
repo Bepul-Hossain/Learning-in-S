5de873a18f393cb78dcb4aa1748bae1aacff8721
bepul-sharetrip <bepul@sharetrip.net>
2024-02-01 14:18:17 +0600

    aa

---
+++ office-work/d5 Priti Cardholders/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const { TaggaClub } = require('./input/tagga.js');
+// const { tagaClub } = require('./input/hello.js');
+
+// console.log(tagaClub);
+// let TaggaClub = tagaClub;
+// console.log(TaggaClub[0]);
+
+let TaggaCodes = {};
+
+// return;
+
+// let count = 0;
+// for (let item of Object.values(TaggaClub)) {
+//     const value = Object.values(item);
+//     count++;
+//     TaggaCodes[value[0]]=value[0];
+// }
+// console.log(Object.keys(TaggaCodes).length);
+// fs.writeFile('./output/TaggaCodes.json', JSON.stringify(TaggaCodes), err => {
+//     if (err) {
+//         console.log('Error writing file', err)
+//     } else {
+//         console.log('Successfully wrote the unique Code and unique Id json file')
+//     }
+// })
---
+++ office-work/d5 Priti Cardholders/input/tagga.js
@@ -0,0 +1,13 @@
+module.exports = {
+  TaggaClub: 
+  [
+    {
+      "Card Number": "001 11516 00000",
+      "Expiry Date": "10\/27"
+     },
+     {
+      "Card Number": "002 15316 00000",
+      "Expiry Date": "10\/27"
+     },
+  ]
+}
